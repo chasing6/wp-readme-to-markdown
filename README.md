d13b545fe15d86f3135c7c0b84ce14ca56d526a7
Scott <chasing6@users.noreply.github.com>
2018-12-16 01:11:42 -0800

    Add file conversion, hooks, readme files

---
+++ README.md
@@ -0,0 +1,60 @@
+# WP Readme to Markdown 
+**Contributors:** (this should be a list of wordpress.org userid's)  
+**Tags:** comments, spam  
+**Requires at least:** 4.9  
+**Tested up to:** 4.9  
+**Stable tag:** 4.6  
+**License:** GPLv2 or later  
+**License URI:** http://www.gnu.org/licenses/gpl-2.0.html  
+
+Converts your readme.txt files into markdown for posting on sites like Github.com.
+
+
+## Description 
+
+This plugin is used in a dev environment to automatically convert WordPress's readme.txt file to a README.md markdown file (for Github and the like). **DO NOT USE THIS PLUGIN IN A PRODUCTION ENVIRONMENT!!!**
+
+
+## Requirements 
+
+Your WP install must be in debug mode for this to work. Please add `define('WP_DEBUG', true);` to your wp-config.php file. You must also have proper read/write permissions in your plugin's folder
+
+
+## Usage 
+
+
+### Adding a File for Conversion 
+
+Adding a file to the convert queue is as easy as adding a single line of code! The plugin uses the hook `wp_rtm/add` to run the function. Paramaters include:
+```php
+/**
+ * wp_rtm/add parameters:
+ *
+ * @param string $filepath    the main path to the plugin readme.txt file.
+ * @param string $namespace   (optional) unique identifier used for this file, used for hooking later. Defaults to numeric key.
+ * @param string $readme      (optional) path to readme.txt file relative to $filepath. Defaults to readme.txt
+ * @param string $markdown    (optional) path to MARKDOWN.md file relative to $filepath. Defaults to README.md
+ */
+```
+
+The simplest way to convert your readme.txt file is to add the following line into the main file of your plugin.
+
+```php
+do_action('wp_rtm/add', plugin_dir_path() );
+```
+In order to allow for hooking into the file directly, it is recommended that you add a namespace to the file queue:
+
+```php
+do_action( 'wp_rtm/add', plugin_dir_path(), 'plugin-name' );
+```
+**Note that if you queue up a file with the same namespace as a file already in the queue, the original file will be overwritten.**
+
+Files are converted during the `plugins_loaded` action, which means that in order to generate the new files you must reload your browser.
+
+
+## Changelog 
+
+
+### 0.2.0 
+**Enhancement** Added `wp_rtm/files` filter.
+**Enhancement** Added `wp_rtm/file-{namespace}/post_save` action.
--- wp-readme-to-markdown.php
+++ wp-readme-to-markdown.php
@@ -9,7 +9,7 @@
  * Plugin Name:       WP Readme to Markdown
  * Plugin URI:        https://github.com/chasing6/wp-readme-to-markdown
  * Description:       Converts the readme.txt file into markdown for github, etc.
- * Version:           0.1.0
+ * Version:           0.2.0
  * Author:            Scott McCoy
  * Author URI:        https://github.com/chasing6/
  * Text Domain:       wp-rtm
@@ -30,29 +30,122 @@ class WP_Readme_To_Markdown{
 	 */
   static $files = array();
 
+  /**
+   * Loads up the hooks for main functionality
+   *
+   * @since 1.0.0
+   */
+
   static function load(){
     add_action( 'wp_rtm/add', array( __CLASS__, 'add'), 10, 4 );
     add_action( 'plugins_loaded', array( __CLASS__, 'convert_files') );
   }
 
-  static function add( $namespace, $filepath, $readme = 'readme.txt', $markdown = 'MARKDOWN.md'){
-    // add the info to the namespace
-    self::$files[ $namespace ] = array(
-      'filepath'      => $filepath,
-      'readme'        => $readme,
-      'markdown'      => $markdown,
-    );
+  /**
+   * Adds a specific file to the convert queue
+   *
+   * @since 1.0.0
+   *
+   * @param string $filepath    the main path to the plugin, use plugin_dir_path(__FILE__) in most cases.
+   * @param string $namespace   (optional) unique identifier used for this file, used for hooking later
+   * @param string $readme      (optional) path to readme.txt file relative to $filepath
+   * @param string $markdown    (optional) path to MARKDOWN.md file relative to $filepath
+   */
 
+  static function add( $filepath, $namespace = NULL, $readme = 'readme.txt', $markdown = 'README.md'){
+
+    if ( is_null( $namespace) ){
+      self::$files[ $namespace ] = array(
+        'filepath'      => $filepath,
+        'readme'        => $readme,
+        'markdown'      => $markdown,
+      );
+    } else {
+      self::$files[] = array(
+        'filepath'      => $filepath,
+        'readme'        => $readme,
+        'markdown'      => $markdown,
+      );
+    }
   }
+
+  /**
+   * Converts all the files in $files from txt to md
+   *
+   * @since 1.0.0
+   */
 
   static function convert_files(){
 
-    $files = apply_filters( 'wp_rtm/files', self::$files );
+    /**
+     * Filter to edit the file queue before paths are built
+     *
+     * @since 1.0.0
+     */
+    self::$files = apply_filters( 'wp_rtm/files', self::$files );
 
-    // TODO: add a way to append or prepend the markdown file
+    // build the file paths for each file in the queue
+    self::build_file_paths();
 
-    // TODO: actually convert the file!
-    do_action( 'add_debug_info', $files, 'Readme to Markdown Conversions');
+    // actually convert the file!
+    foreach ( self::$files as $namespace => $file ){
+
+      if( file_exists( $file['txt_path'] ) ){
+        $txt_contents = file_get_contents( $file['txt_path'] );
+
+        // convert txt to markdown
+        $markdown = \WPReadme2Markdown\Converter::convert( $txt_contents );
+
+        // write/create the file
+        if ( ! file_exists( $file['md_path']) || self::check_contents( $file['md_path'], $markdown ) ) {
+          self::save_markdown_file( $namespace, $markdown );
+        }
+      }
+    }
+  }
+
+  /**
+   * Converts all the files in $files from txt to md
+   *
+   * @since 1.0.0
+   */
+  private function build_file_paths(){
+    foreach ( self::$files as $ns => $file ){
+      self::$files[$ns]['txt_path'] = $file['filepath'] . $file['readme'];
+      self::$files[$ns]['md_path'] = $file['filepath'] . $file['markdown'];
+    }
+  }
+
+  private function check_contents( $md_path, $contents ){
+    // if the file doesn't exist yet, write one
+    if (! file_exists($md_path) ){
+      return true;
+    }
+
+    $old_contents = file_get_contents($md_path);
+
+    if ( $old_contents != $contents ){
+      return true;
+    }
+    return false;
+  }
+
+  private function save_markdown_file( $namespace, $contents ){
+    $markdown_file = fopen( self::$files[$namespace]['md_path'], 'w');
+
+    // allow a filter if a namespace was given
+    if ( is_string( $namespace ) ) {
+      $contents = apply_filters( 'wp_rtm/file_' . $namespace . '/pre_save/md_contents', $contents );
+    }
+
+    fwrite($markdown_file, $contents);
+    fclose($markdown_file);
+
+    // allow a post_save action if a namespace was given
+    if ( is_string( $namespace ) ) {
+      do_action( 'wp_rtm/file_' . $namespace . '/post_save', $contents );
+    }
+
   }
 
 }
@@ -60,7 +153,7 @@ class WP_Readme_To_Markdown{
 WP_Readme_To_Markdown::load();
 
 // let's convert this plugin's file!
-do_action( 'wp_rtm/add', 'wp-rtm', plugin_dir_path(__FILE__) );
+do_action( 'wp_rtm/add', plugin_dir_path(__FILE__), 'wp_rtm' );
 
 // make sure this is the first plugin loaded,
 // so that we can use the wp_rtm/add action filter reliably;
