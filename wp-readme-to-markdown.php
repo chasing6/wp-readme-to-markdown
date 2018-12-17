5ced49a429bbfb9ccb7fa26a56203e586d216c28
Scott <chasing6@users.noreply.github.com>
2018-12-17 15:51:05 -0800

    Refactor for more easily adding hooks

--- wp-readme-to-markdown.php
+++ wp-readme-to-markdown.php
@@ -31,6 +31,14 @@ class WP_Readme_To_Markdown{
   static $files = array();
 
   /**
+   * Array of built files
+   *
+   * @var array
+   */
+
+   static $built_files = array();
+
+  /**
    * Loads up the hooks for main functionality
    *
    * @since 1.0.0
@@ -54,7 +62,7 @@ class WP_Readme_To_Markdown{
 
   static function add( $filepath, $namespace = NULL, $readme = 'readme.txt', $markdown = 'README.md'){
 
-    if ( is_null( $namespace) ){
+    if ( is_string( $namespace) ){
       self::$files[ $namespace ] = array(
         'filepath'      => $filepath,
         'readme'        => $readme,
@@ -77,6 +85,11 @@ class WP_Readme_To_Markdown{
 
   static function convert_files(){
 
+    // Only allow admin users to gen files
+    if ( current_user_can('administrator') ) {
+      //return;
+    }
+
     /**
      * Filter to edit the file queue before paths are built
      *
@@ -84,39 +97,27 @@ class WP_Readme_To_Markdown{
      */
     self::$files = apply_filters( 'wp_rtm/files', self::$files );
 
-    // build the file paths for each file in the queue
-    self::build_file_paths();
-
     // actually convert the file!
-    foreach ( self::$files as $namespace => $file ){
+    foreach ( self::$files as $ns => $file ){
 
-      if( file_exists( $file['txt_path'] ) ){
-        $txt_contents = file_get_contents( $file['txt_path'] );
+      self::run_convert( $ns );
 
-        // convert txt to markdown
-        $markdown = \WPReadme2Markdown\Converter::convert( $txt_contents );
-
-        // write/create the file
-        if ( ! file_exists( $file['md_path']) || self::check_contents( $file['md_path'], $markdown ) ) {
-          self::save_markdown_file( $namespace, $markdown );
-        }
+      if ( self::file_changes( $ns ) ){
+        self::save_file( $ns );
       }
     }
   }
 
   /**
-   * Converts all the files in $files from txt to md
+   * Checks to see if the contents of readme.txt is different than what is already
+   * in the README.md file
    *
    * @since 1.0.0
+   *
+   * @param string $md_path   the path to the md file
+   * @param string $contents  the contents of the txt file
    */
-  private function build_file_paths(){
-    foreach ( self::$files as $ns => $file ){
-      self::$files[$ns]['txt_path'] = $file['filepath'] . $file['readme'];
-      self::$files[$ns]['md_path'] = $file['filepath'] . $file['markdown'];
-    }
-  }
-
-  private function check_contents( $md_path, $contents ){
+  private static function check_contents( $md_path, $contents ){
     // if the file doesn't exist yet, write one
     if (! file_exists($md_path) ){
       return true;
@@ -130,30 +131,141 @@ class WP_Readme_To_Markdown{
     return false;
   }
 
-  private function save_markdown_file( $namespace, $contents ){
-    $markdown_file = fopen( self::$files[$namespace]['md_path'], 'w');
+  private static function file_changes( $ns ){
 
-    // allow a filter if a namespace was given
-    if ( is_string( $namespace ) ) {
-      $contents = apply_filters( 'wp_rtm/file_' . $namespace . '/pre_save/md_contents', $contents );
+    return true;
+    if ( self::$built_files[$ns]['converted'] != self::$built_files[$ns]['md_contents'] ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private static function save_file( $ns ){
+
+    $attr = self::$built_files[$ns];
+
+    if ( is_string( $ns ) ){
+      do_action( 'wp_rtm/file/' . $ns . '/before_save_md', $ns, self::$built_files[$ns] );
     }
 
-    fwrite($markdown_file, $contents);
-    fclose($markdown_file);
+    // open the file
+    $file = fopen( self::$built_files[$ns]['md_path'], 'w');
+    fwrite($file, self::$built_files[$ns]['converted'] );
+    fclose($file);
 
-    // allow a post_save action if a namespace was given
-    if ( is_string( $namespace ) ) {
-      do_action( 'wp_rtm/file_' . $namespace . '/post_save', $contents );
+
+    if ( is_string( $ns ) ){
+      do_action( 'wp_rtm/file/' . $ns . '/after_save_md', $ns, self::$built_files[$ns] );
     }
 
   }
 
+  private static function run_convert( $ns ){
+    self::build_file_paths( $ns );
+    self::get_file_contents( $ns );
+    self::convert_to_md( $ns );
+  }
+
+  private static function build_file_paths( $ns ){
+    $file = self::$files[$ns];
+
+    $build = array(
+      'txt_path'  => $file['filepath'] . $file['readme'],
+      'md_path'   => $file['filepath'] . $file['markdown'],
+    );
+
+    self::$built_files[$ns] = $build;
+  }
+
+  private static function get_file_contents( $ns ){
+
+    $file = self::$built_files[$ns];
+
+    if ( file_exists( $file['txt_path']) ){
+      $file['txt_contents'] = file_get_contents( $file['txt_path'] );
+    } else {
+      $file['txt_contents'] = 'No file found at ' . $file['text_path'];
+    }
+
+    if ( file_exists( $file['md_path']) ){
+      $file['md_contents'] = file_get_contents( $file['md_path'] );
+    } else {
+      $file['md_contents'] = 'No markdown File';
+    }
+
+    self::$built_files[$ns] = $file;
+
+  }
+
+  private static function convert_to_md( $ns ){
+
+    $file = self::$built_files[$ns];
+
+    // TODO: Add prepend file hook
+
+    $raw = \WPReadme2Markdown\Converter::convert( $file['txt_contents'] );
+
+    $hooked = self::markdown_toc($raw);
+
+    // convert txt to markdown
+    $file['converted'] = $hooked;
+
+    self::$built_files[$ns] = $file;
+
+  }
+
+  private static function markdown_toc($file_path) {
+    //$file = file_get_contents($file_path);
+
+    $file = $file_path;
+
+    // ensure using only "\n" as line-break
+    $source = str_replace(["\r\n", "\r"], "\n", $file);
+
+    // look for markdown TOC items
+    preg_match_all(
+      '/^(?:=|-|#).*$/m',
+      $source,
+      $matches,
+      PREG_PATTERN_ORDER | PREG_OFFSET_CAPTURE
+    );
+
+    // preprocess: iterate matched lines to create an array of items
+    // where each item is an array(level, text)
+    $file_size = strlen($source);
+    foreach ($matches[0] as $item) {
+      $found_mark = substr($item[0], 0, 1);
+      if ($found_mark == '#') {
+        // text is the found item
+        $item_text = $item[0];
+        $item_level = strrpos($item_text, '#') + 1;
+        $item_text = substr($item_text, $item_level);
+      } else {
+        // text is the previous line (empty if <hr>)
+        $item_offset = $item[1];
+        $prev_line_offset = strrpos($source, "\n", -($file_size - $item_offset + 2));
+        $item_text =
+          substr($source, $prev_line_offset, $item_offset - $prev_line_offset - 1);
+        $item_text = trim($item_text);
+        $item_level = $found_mark == '=' ? 1 : 2;
+      }
+      if (!trim($item_text) OR strpos($item_text, '|') !== FALSE) {
+        // item is an horizontal separator or a table header, don't mind
+        continue;
+      }
+      $raw_toc[] = ['level' => $item_level, 'text' => trim($item_text)];
+    }
+    var_dump( $raw_toc);
+
+    return $file;
+  }
 }
 
 WP_Readme_To_Markdown::load();
 
 // let's convert this plugin's file!
-do_action( 'wp_rtm/add', plugin_dir_path(__FILE__), 'wp_rtm' );
+do_action( 'wp_rtm/add', plugin_dir_path(__FILE__), 'wp_rtm', 'readme.txt', 'README.MD' );
 
 // make sure this is the first plugin loaded,
 // so that we can use the wp_rtm/add action filter reliably;
@@ -169,4 +281,4 @@ function load_wp_rtm_first() {
 		update_option('active_plugins', $active_plugins);
 	}
 }
-add_action("activated_plugin", "load_wp_rtm_first");
+add_action( 'activated_plugin', 'load_wp_rtm_first' );
